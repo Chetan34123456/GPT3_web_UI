89d905851a6889a13f9e3642777ece564ebdf386
chetan <chetanhiwale67@gmail.com>
2022-12-28 00:49:30 +0530

    header image

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
                 </div>
             </div>
             <div className="gpt3__header-image">
-                <img src={ai} />
+                <img src={ai} alt="image" />
             </div>
         </div>
     )
